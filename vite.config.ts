3759ac45ee420d20ded1683c64082edc737fe678
zxzDENISzxz <tumanov.denis78@gmail.com>
2026-06-16 18:35:36 +0500

    Fix repository case in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/codequest/',
+  base: '/CODEQUEST/',
   plugins: [
     react(),
     tailwindcss(),
